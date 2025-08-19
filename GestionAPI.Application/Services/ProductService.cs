23b722719b921fad4a8cb1164589de19131d30e8
Francisco Galea <galeafrancisco2001@gmail.com>
2025-08-19 17:37:30 -0300

    Implementing services

--- GestionAPI.Application/Services/ProductService.cs
+++ GestionAPI.Application/Services/ProductService.cs
@@ -1,8 +1,52 @@
-﻿using GestionAPI.Application.Interfaces.IService;
+﻿using GestionAPI.Application.DTOs.Request;
+using GestionAPI.Application.Interfaces.IRepository;
+using GestionAPI.Application.Interfaces.IService;
+using GestionAPI.Domain.Entities;
 
 namespace GestionAPI.Application.Services
 {
     public class ProductService : IProductService
     {
+
+        private readonly IProductRepository repository;
+
+        public ProductService(IProductRepository repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task CreateProduct(ProductRequest productRequest)
+        {
+            ProductEntity product = new ProductEntity() { 
+                ProductName = productRequest.ProductName, 
+                ProductPrice = productRequest.ProductPrice
+            };
+            await repository.InsertAsync(product);
+        }
+
+        public Task DeleteProduct(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<ProductEntity>> GetAllProducts()
+        {
+            return await repository.GetAllAsync();
+        }
+
+        public async Task<ProductEntity> GetProductById(int productId)
+        {
+            return await repository.GetByIdAsync(productId);
+        }
+
+        public async Task UpdateProduct(int productId, ProductRequest productRequest)
+        {
+            ProductEntity product = new ProductEntity() {  
+                ProductId = productId, 
+                ProductName = productRequest.ProductName, 
+                ProductPrice = productRequest.ProductPrice 
+            };
+            await repository.UpdateAsync(product);
+        }
     }
 }
