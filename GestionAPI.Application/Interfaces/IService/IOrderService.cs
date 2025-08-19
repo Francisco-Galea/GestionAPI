80ef530d2ed3588d34a5d47ec332ca84be252c53
Francisco Galea <galeafrancisco2001@gmail.com>
2025-08-19 17:37:10 -0300

    Implementing DI principle

---
+++ GestionAPI.Application/Interfaces/IService/IOrderService.cs
@@ -0,0 +1,14 @@
+﻿using GestionAPI.Application.DTOs.Request;
+using GestionAPI.Domain.Entities;
+
+namespace GestionAPI.Application.Interfaces.IService
+{
+    public interface IOrderService
+    {
+        Task CreateOrder(OrderRequest orderRequest);
+        Task UpdateOrder(int orderId, OrderRequest orderRequest);
+        Task<OrderEntity> GetOrderById(int orderId);
+        Task<List<OrderEntity>> GetAllOrders(); 
+        Task DeleteOrder(int orderId);
+    }
+}
--- GestionAPI.Application/Interfaces/IService/IProductService.cs
+++ GestionAPI.Application/Interfaces/IService/IProductService.cs
@@ -1,7 +1,14 @@
-﻿
+﻿using GestionAPI.Application.DTOs.Request;
+using GestionAPI.Domain.Entities;
+
 namespace GestionAPI.Application.Interfaces.IService
 {
     public interface IProductService
     {
+        Task CreateProduct(ProductRequest productRequest);
+        Task UpdateProduct(int productId, ProductRequest productRequest);
+        Task<ProductEntity> GetProductById(int productId);
+        Task<List<ProductEntity>> GetAllProducts();
+        Task DeleteProduct(int productId);
     }
 }
