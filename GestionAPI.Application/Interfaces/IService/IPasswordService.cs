2fd6bbd7ea10f06adf28ffc3b5f8dfdfa8b033d9
Francisco Galea <galeafrancisco2001@gmail.com>
2025-08-21 19:18:42 -0300

    Implemented User and Password service/repository

---
+++ GestionAPI.Application/Interfaces/IService/IPasswordService.cs
@@ -0,0 +1,9 @@
+﻿
+namespace GestionAPI.Application.Interfaces.IService
+{
+    public interface IPasswordService
+    {
+        Task<string> HashPassword(string password);
+        Task<bool> VerifyPassword(string password, string hash);
+    }
+}
---
+++ GestionAPI.Application/Interfaces/IService/IUserService.cs
@@ -0,0 +1,14 @@
+﻿using GestionAPI.Application.DTOs.Request;
+using GestionAPI.Domain.Entities;
+
+namespace GestionAPI.Application.Interfaces.IService
+{
+    public interface IUserService
+    {
+        Task CreateUser(UserRequest userRequest);
+        Task UpdateUser(int userId, UserRequest userRequest);
+        Task<UserEntity> GetUserById(int userId);
+        Task<List<UserEntity>> GetAllUsers();
+        Task DeleteUser(int userId);
+    }
+}
