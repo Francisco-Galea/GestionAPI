b8b80c4062f2b9c218d1f8258a98c879eac40335
Francisco Galea <galeafrancisco2001@gmail.com>
2025-08-21 19:19:24 -0300

    Implemented Password and User service

---
+++ GestionAPI.Application/Services/PasswordService.cs
@@ -0,0 +1,18 @@
+﻿using GestionAPI.Application.Interfaces.IService;
+
+namespace GestionAPI.Application.Services
+{
+    public class PasswordService : IPasswordService
+    {
+        public async Task<string> HashPassword(string password)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(password);
+        }
+
+        public async Task<bool> VerifyPassword(string password, string hash)
+        {
+            return BCrypt.Net.BCrypt.Verify(password, hash);
+        }
+
+    }
+}
---
+++ GestionAPI.Application/Services/UserService.cs
@@ -0,0 +1,57 @@
+﻿using GestionAPI.Application.DTOs.Request;
+using GestionAPI.Application.Interfaces.IRepository;
+using GestionAPI.Application.Interfaces.IService;
+using GestionAPI.Domain.Entities;
+
+namespace GestionAPI.Application.Services
+{
+    public class UserService : IUserService
+    {
+
+        private readonly IUserRepository userRepository;
+        private readonly IPasswordService passwordService;
+
+        public UserService(IUserRepository userRepository, IPasswordService passwordService)
+        {
+            this.userRepository = userRepository;
+            this.passwordService = passwordService;
+        }
+
+        public async Task CreateUser(UserRequest userRequest)
+        {
+            UserEntity userEntity = new UserEntity()
+            {
+                UserName = userRequest.UserName,
+                UserEmail = userRequest.UserMail,
+                UserPassword = await passwordService.HashPassword(userRequest.UserPassword)
+            };
+            await userRepository.InsertAsync(userEntity);
+        }
+
+        public Task DeleteUser(int userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<UserEntity>> GetAllUsers()
+        {
+            return await userRepository.GetAllAsync();
+        }
+
+        public async Task<UserEntity> GetUserById(int userId)
+        {
+            return await userRepository.GetByIdAsync(userId);
+        }
+
+        public async Task UpdateUser(int userId, UserRequest userRequest)
+        {
+            UserEntity userEntity = new UserEntity()
+            {
+                UserName = userRequest.UserName,
+                UserEmail = userRequest.UserMail,
+                UserPassword = await passwordService.HashPassword(userRequest.UserPassword)
+            };
+            await userRepository.UpdateAsync(userEntity);
+        }
+    }
+}
