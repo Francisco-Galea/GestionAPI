741ba1396bd82dc64e64c2d6b1dd3aceb1b85f59
Francisco Galea <galeafrancisco2001@gmail.com>
2025-08-25 13:36:45 -0300

    Test migrations

---
+++ GestorAPI.Infrastructure/Migrations/20250822211129_productDescriptionMigration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GestionAPI.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class productDescriptionMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ProductDescription",
+                table: "Products",
+                type: "nvarchar(256)",
+                maxLength: 256,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ProductDescription",
+                table: "Products");
+        }
+    }
+}
