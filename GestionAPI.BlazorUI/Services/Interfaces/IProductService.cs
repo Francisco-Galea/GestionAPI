af96bf79dc82ad93275169298cc56b16f418bd63
Francisco Galea <galeafrancisco2001@gmail.com>
2025-08-25 13:36:29 -0300

    implementing ProductService

---
+++ GestionAPI.BlazorUI/Services/Interfaces/IProductService.cs
@@ -0,0 +1,14 @@
+﻿using GestionAPI.BlazorUI.Models.DTOs.Request;
+using GestionAPI.BlazorUI.Models.DTOs.Response;
+
+namespace GestionAPI.BlazorUI.Services.Interfaces
+{
+    public interface IProductService
+    {
+        Task CreateProduct(ProductRequest productRequest);
+        Task<List<ProductResponse>> GetAllProducts();
+        Task<ProductResponse> GetProductById(int productId);
+        Task DeleteProduct(int productId);
+        Task UpdateProduct(int productId, ProductResponse product);
+    }
+}
---
+++ GestionAPI.BlazorUI/Services/ProductService.cs
@@ -0,0 +1,44 @@
+﻿using GestionAPI.BlazorUI.Models.DTOs.Request;
+using GestionAPI.BlazorUI.Models.DTOs.Response;
+using GestionAPI.BlazorUI.Services.Interfaces;
+
+namespace GestionAPI.BlazorUI.Services
+{
+    public class ProductService : IProductService
+    {
+
+        private readonly HttpClient httpClient;
+
+        public ProductService(IHttpClientFactory httpClientFactory)
+        {
+            this.httpClient = httpClientFactory.CreateClient("GestionAPI");
+        }
+
+        public Task CreateProduct(ProductRequest productRequest)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteProduct(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<List<ProductResponse>> GetAllProducts()
+        {
+            List<ProductResponse> products = new List<ProductResponse>();
+            products = await httpClient.GetFromJsonAsync<List<ProductResponse>>("api/products");
+            return products;
+        }
+
+        public Task<ProductResponse> GetProductById(int productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateProduct(int productId, ProductResponse product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
