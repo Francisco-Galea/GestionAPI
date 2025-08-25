4ba19f2cdf8c7848027be3a7eb7e73edbd99402c
Francisco Galea <galeafrancisco2001@gmail.com>
2025-08-25 13:36:12 -0300

    Testing Product request/response

---
+++ GestionAPI.BlazorUI/Models/DTOs/Request/ProductRequest.cs
@@ -0,0 +1,10 @@
+﻿namespace GestionAPI.BlazorUI.Models.DTOs.Request
+{
+    public class ProductRequest
+    {
+        public string ProductName { get; set; } = string.Empty;
+        public string ProductDescription { get; set; } = string.Empty;
+        public int ProductStock {  get; set; }
+        public decimal ProductPrice { get; set; }
+    }
+}
---
+++ GestionAPI.BlazorUI/Models/DTOs/Response/ProductResponse.cs
@@ -0,0 +1,11 @@
+﻿namespace GestionAPI.BlazorUI.Models.DTOs.Response
+{
+    public class ProductResponse
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; } = string.Empty;
+        public string ProductDescription { get; set; } = string.Empty;
+        public int ProductStock { get; set; }
+        public decimal ProductPrice { get; set; }
+    }
+}
